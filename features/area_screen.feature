807ff01714b5d16367caa58f09429a57c162a806
Sakardin <sakardin@tut.by>
2018-03-29 12:35:48 +0300

    Home_Work_2 Lesson_2 Assertions for all tests.

--- features/area_screen.feature
+++ features/area_screen.feature
@@ -1,22 +1,23 @@
 @area
 Feature: User is able to convert area units
-  @menu
-  Scenario: User is able to dismiss help
-    Given I land on help popup
-    When I click on Got it button
-    Then I land on Area screen
+
+  Background:
+    Given I click on Got it button
+
+#  Scenario: User is able to dismiss help
+#    Given I land on help popup
+#    When I click on Got it button
+#    Then I land on Area screen
 
     Scenario:  User is able to swap values
-      Given I click on Got it button
-      And I see "Sq Kilometre" in From header
+      Given I see "Sq Kilometre" in From header
       And I see "Sq Metre"  in To header
       When I click on Swap button
       Then I see "Sq Metre" in From header
       And I see "Sq Kilometre"  in To header
-@wip
+
       Scenario Outline: User is able to convert default units
-        Given I click on Got it button
-        And I click on Clear button
+        Given I click on Clear button
         When I enter "<target>" to From field
         Then i get "<result>" in To field
 
@@ -25,6 +26,18 @@ Feature: User is able to convert area units
         |1     |1000000|
         |2     |2000000|
         |3     |3000000|
+@wip
+    Scenario: User is able to use soft keyboard to enter values
+      Given I click on Clear button
+      When I click on From from field
+      And I press "12" on soft keyboard
+      Then i get "12000000" in To field
+@wip
+    Scenario:
+      When I select "Hectare" from left column
+      Then I see "Hectare" in From header
+      And i get "10000" in To field
+
 
 
 
--- features/step_definitions/area_screen_steps.rb
+++ features/step_definitions/area_screen_steps.rb
@@ -1,43 +1,73 @@
 Given(/^I land on help popup$/) do
-  puts("User lands on Help popup")
+  texr("Help")
+
 end
 
 When(/^I click on Got it button$/) do
-  puts("User clicks on Got it button")
+  find_element(id: "button1").click
 end
 
 Then(/^I land on Area screen$/) do
-  puts("User lands on Area screen")
+  texr("Area")
 end
 
-And(/^I see Sq Kilometre in From header$/) do
-  puts("User sees Sq Kilometre")
-end
-
-And(/^I see Sq Metre  in To header$/) do
-  puts("User sees Sq Metre")
-end
+# And(/^I see Sq Kilometre in From header$/) do
+#   puts("User sees Sq Kilometre")
+# end
+#
+# And(/^I see Sq Metre  in To header$/) do
+#   puts("User sees Sq Metre")
+# end
 
 When(/^I click on Swap button$/) do
-  puts("User clicks Swap button")
+  find_element(id:"fab").click
 end
 
 Then(/^I see "([^"]*)" in From header$/) do |value|
-  puts("From header value is " +value)
+  actual_value = find_element(id: "header_text_unit_from").text
+  if actual_value != value
+    fail("Expected value is #{value} but actual is #{actual_value}")
+  end
 end
 
 And(/^I see "([^"]*)"  in To header$/) do |value|
-  puts("From header value is #{value}")
+  actual_value = find_element(id: "header_text_unit_to").text
+  if actual_value != value
+    fail("Expected value is #{value} but actual is #{actual_value}")
+  end
 end
 
+
 And(/^I click on Clear button$/) do
-  puts("User clicks on Clear button")
+  find_element(id:"menu_clear").click
 end
 
 When(/^I enter "([^"]*)" to From field$/) do |value|
-  puts("User entered value #{value}")
+  find_element(id: "header_value_from").send_keys(value)
 end
 
 Then(/^i get "([^"]*)" in To field$/) do |value|
-  puts("User sees #{value} in result field")
+  actual_value = find_element(id: "header_value_to").text
+    if actual_value != value
+    fail("Expected value is #{value} but actual is #{actual_value}")
+  end
+end
+
+When(/^I click on From from field$/) do
+  find_element(id: "header_value_from").click
+end
+
+And(/^I press "([^"]*)" on soft keyboard$/) do |value|
+  digits = value.split("")
+  digits.each do |key|
+  digit = Integer(key)
+  press_keycode 7 + digit
+  end
+
+end
+
+
+When(/^I select "([^"]*)" from left column$/) do |value|
+  find_element(id:"radio_group_from").find_element(xpath: "//android.widget.RadioButton[@text='#{value}']").click
+
 end
